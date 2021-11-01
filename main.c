92e8ec7a7523313c575100849b81a0b63081a6bd
Danil <68150308+mo0nchild@users.noreply.github.com>
2021-11-01 18:53:21 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "coursework.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+int main(void) 
+{
+	start_app();
+
+	/*for(int i = 0; i < 120; i++) 
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), i);
+		printf("%d\n", i);
+	}*/
+
+	return 0;
+}
